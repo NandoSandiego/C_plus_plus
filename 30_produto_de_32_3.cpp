6745e25c3e5afbda2c2976114a48fe73cf264432
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-11 16:43:17 -0300

    up 30

---
+++ 30_produto_de_32_3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int num_1 = 23, num_2 = 3;
+
+int main (){
+	
+	cout <<"A multiplicacao de " << num_1 << " e " << num_2 << " é: "<< num_1*num_2;
+	return 0;
+}

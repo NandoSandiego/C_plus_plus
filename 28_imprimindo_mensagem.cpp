6dff3e2b23fe9c68424649dff6b855963245b43f
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-11 16:38:47 -0300

    up 11/ 04

---
+++ 28_imprimindo_mensagem.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // permite que o programa gere saída de dados na tela
+using namespace std;
+
+int main (){
+	
+	
+	cout << "Hello World \n"; 
+	
+	return 0;
+}
---
+++ 29_imprimindo_nome.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // permite que o programa realize entrada e saída
+using namespace std;
+
+
+int main (){
+	
+	string name;
+	cout << "Digite o seu nome: " ;
+	cin >> name ;
+	
+	cout << "Ola " << name << ", tudo bem ?" ;
+	
+	
+	return 0;
+}
+

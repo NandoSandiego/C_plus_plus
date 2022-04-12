7871aea7bdf2600165d12c300acbada3ac17e191
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-12 17:31:45 -0300

    37

---
+++ 37_multiplica.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+	
+	int num_a, num_b;
+	
+	cout << "Digite um numero: ";
+	cin >> num_a;
+	cout << "Digite outro numero: ";
+	cin >> num_b;
+	
+	cout << num_a << " X " << num_b << " = " << num_a * num_b;
+	
+	
+	
+	return 0;
+}

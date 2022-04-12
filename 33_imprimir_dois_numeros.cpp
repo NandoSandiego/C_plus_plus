31e6f572c81a5254c91f3c064952cf8df1127f16
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-11 21:34:14 -0300

    33

---
+++ 33_imprimir_dois_numeros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+ int main(){
+	
+	int num_1, num_2;
+	
+	cout << "Digite um numero: " ;
+	cin >> num_1 ;
+	cout << "Digite outro numro: " ;
+	cin >> num_2 ;
+	
+	cout <<"Os numeros digitados  foram " << num_1 << " e " << num_2;
+	
+	
+	return 0;
+}

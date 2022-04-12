49609c824353717a4acb68da5c69393b77108495
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-12 17:27:47 -0300

    36

---
+++ 36_soma.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+	
+	int num_a, num_b;
+	
+	cout << "Digite um numero: ";
+	cin >> num_a;
+	cout << "Digite outro numero: ";
+	cin >> num_b;
+	
+	cout << "A soma de: " << num_a << " + " << num_b << " = " << num_a+num_b;
+	
+	
+	
+	
+	return 0;
+}

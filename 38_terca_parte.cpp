30d74a403690c1533da57f07a4e70cb4eb11bc72
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-12 17:36:02 -0300

    38

---
+++ 38_terca_parte.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+	
+	float num, terca;
+		
+	cout << "Digite um numero: ";
+	cin >> num;
+	
+	terca = num/3;
+	
+	cout << "A terca parte e: " <<terca;
+	
+	
+	
+	
+	return 0;
+}

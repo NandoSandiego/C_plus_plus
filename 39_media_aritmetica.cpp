c24989d78ec70d00de8ca9c1880802bc9c239ee1
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-12 17:45:11 -0300

    39

---
+++ 39_media_aritmetica.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	float num_a, num_b;
+	float media;
+	
+	cout << "Digite um numero: ";
+	cin >> num_a;
+	cout << "Digite outro numero: ";
+	cin >> num_b;
+	
+	media = (num_a + num_b)/2;
+	
+	cout << "A media e: " << media;
+	
+	
+	
+	
+	
+	return 0;
+}

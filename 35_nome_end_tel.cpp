bafe607e3f569e9234abebacfa888217546ca78b
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-12 17:14:02 -0300

    35

--- 35_nome_end_tel.cpp
+++ 35_nome_end_tel.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <stdio.h>
+#include <string>
 using namespace std;
 
 
 int main (){
 	
 	string nome, end, tel;
-	
+		
 	cout << "Digite o nome: ";
 	cin >> nome;
 	cout << "Digite o endereco: ";
@@ -13,9 +15,9 @@ int main (){
 	cout << "Digite o telefone: ";
 	cin >> tel;
 	
-	cout << "Ola, " << nome  << " !";
-	cout << "Voce mora em " << end ;
-	cout << "O telefone é: " << tel ;
+	cout << "Ola, " << nome  << " ! \n";
+	cout << "Voce mora em " << end << "\n";
+	cout << "O telefone e: " << tel ;
 	
 	
 	return 0;

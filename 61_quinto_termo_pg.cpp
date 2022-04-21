784904a09799a3908c741580794dda340072f510
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-21 20:08:24 -0300

    up 61

---
+++ 61_quinto_termo_pg.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+	
+	float razao, pri_termo, ene_termo, num_termo;
+	
+	printf("Digite a razao: ");
+	scanf("%f", &razao);
+	printf("O valor do primeiro termo: ");
+	scanf("%f", &pri_termo);
+	
+	
+	ene_termo =  pri_termo * pow(razao , 4) ;
+	
+	printf("%.2f", ene_termo);
+	
+	
+	
+	
+	return EXIT_SUCCESS;
+}

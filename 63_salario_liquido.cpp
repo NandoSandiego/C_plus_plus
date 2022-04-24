b6a51f11ec18539d3d824bf5b48334436c928665
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-24 00:04:49 -0300

    up 63

--- 63_salario_liquido.cpp
+++ 63_salario_liquido.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 	
-	float valor_hora, num_aulas, percentual;
+	float valor_hora, num_aulas, percentual, valor_bruto, valor_liquido;
 	
 	printf("Digite o valor da hora aula: ");
 	scanf("%f", &valor_hora);
@@ -11,6 +11,13 @@ int main(){
 	scanf("%f", &num_aulas);
 	printf("Qual o percentual: ");
 	scanf("%f", &percentual);
+	
+	valor_bruto = valor_hora * num_aulas;
+	valor_liquido = valor_bruto - valor_bruto * (percentual/100 ) ;
+	
+	printf("--------------------------------\n");
+	printf("O valor bruto: %.2f \n", valor_bruto);
+	printf("O valor liquido e: %.2f", valor_liquido);
 	
 	
 	

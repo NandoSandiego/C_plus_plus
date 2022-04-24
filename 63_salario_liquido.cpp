aa117c74462ba449e85ac481827af466c68c0348
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-23 23:53:24 -0300

    Create 63_salario_liquido.cpp

---
+++ 63_salario_liquido.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	float valor_hora, num_aulas, percentual;
+	
+	printf("Digite o valor da hora aula: ");
+	scanf("%f", &valor_hora);
+	printf("Numero de aulas: ");
+	scanf("%f", &num_aulas);
+	printf("Qual o percentual: ");
+	scanf("%f", &percentual);
+	
+	
+	
+	
+	
+	
+	return EXIT_SUCCESS;
+}

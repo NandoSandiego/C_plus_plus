2d6bcc1331b79a828e65833cb4ae4144debc3564
Fernando Sandiego <mestre.physics@gmail.com>
2022-04-29 16:38:43 -0300

    up 90

---
+++ 90_maior_20.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	
+	int numero;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &numero);
+	
+	if(numero > 20){
+		printf("O numero digitado e maior que 20");
+	}
+	
+	
+	
+	return EXIT_SUCCESS;
+}

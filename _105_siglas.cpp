a2d7719ea1a10874374675bdc7ff3f108f2ec397
Fernando Sandiego <mestre.physics@gmail.com>
2022-05-07 11:45:50 -0300

    up 105

---
+++ _105_siglas.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main (){
+	
+	char sigla[3];
+	int res;
+	
+	printf("Digite a sigla: ");
+	scanf("%s", sigla);
+	//gets(sigla);
+	
+	printf("%s \n", sigla);
+	
+	//res = strcmp(sigla, "pa");
+	
+	
+	if( stricmp(sigla, "PA") == 0 ) { // stricmp: compara ignorando letras maiúsculas/minúsculas 
+		printf("Paraense \n");
+	} else{
+		if(stricmp(sigla, "SC") == 0){
+			printf("Catarinense \n");
+		} else{
+			if( stricmp(sigla, "SP" ) == 0 ){
+				printf("Paulista \n");
+			} else {
+				if(stricmp(sigla, "MG") == 0){
+					printf("Mineiro \n");
+				}else{
+					printf("Outro estado \n");
+				}
+			}
+		}	
+	
+	 } 
+	
+	
+	
+	
+	
+	
+	
+	return EXIT_SUCCESS;
+}

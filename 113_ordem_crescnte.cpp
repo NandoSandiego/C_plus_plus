26b706ae26cc76e548e1750d8c22ee7d53dc784b
Fernando Sandiego <mestre.physics@gmail.com>
2022-05-09 10:50:56 -0300

    up 113

---
+++ 113_ordem_crescnte.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int num_a, num_b;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &num_a);
+	printf("Digite outro numero: ");
+	scanf("%d", &num_b);
+	
+	if(num_a > numb){
+		printf("%d \n", num_a);
+		printf("%d \n", num_b);
+	}else{
+		printf("%d \n", num_b);
+		printf("%d \n", num_a);
+	}
+	
+	
+	
+	return EXIT_SUCCESS	;
+}

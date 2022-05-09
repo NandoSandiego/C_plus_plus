485da301bf7585646248d0f31872347afad97866
Fernando Sandiego <mestre.physics@gmail.com>
2022-05-09 10:54:58 -0300

    up 114

--- 114_ordem_decrescente.cpp
+++ 114_ordem_decrescente.cpp
@@ -5,20 +5,21 @@ int main(){
 	
 	int num_a, num_b;
 	
+	
 	printf("Digite um numero: ");
 	scanf("%d", &num_a);
 	printf("Digite outro numero: ");
 	scanf("%d", &num_b);
 	
 	if(num_a > num_b){
-		printf("%d \n", num_a);
 		printf("%d \n", num_b);
+		printf("%d \n", num_a);
 	}else{
-		printf("%d \n", num_b);
 		printf("%d \n", num_a);
+		printf("%d \n", num_b);
 	}
 	
 	
 	
-	return EXIT_SUCCESS	;
+	return EXIT_SUCCESS;
 }

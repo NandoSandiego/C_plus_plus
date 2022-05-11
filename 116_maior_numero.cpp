e35a3cf0c7c8be0998d066fe674d4511927ff9a5
Fernando Sandiego <mestre.physics@gmail.com>
2022-05-11 10:37:22 -0300

    up 116

---
+++ 116_maior_numero.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (){
+	
+	int num_a, num_b, num_c, maior;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &num_a);
+	printf("Digite outro numero: ");
+	scanf("%d", &num_b);
+	printf("Digite o ultimo numero: ");
+	scanf("%d", &num_c);
+	
+	if(num_a > num_b && num_a > num_c){
+		maior = num_a;
+	} else if (num_b> num_c){
+		maior = num_b;
+	}else{
+		maior = num_c;
+	}
+	
+	printf("%d",maior);
+	
+	
+	
+	
+	
+	return EXIT_SUCCESS;
+}

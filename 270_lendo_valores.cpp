538e9132870fb2509a1151329492249f2199a7b7
Fernando Sandiego <mestre.physics@gmail.com>
2022-05-17 14:32:00 -0300

    up 270

---
+++ 270_lendo_valores.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	int  num;
+	
+	do{
+		printf("Digite um numero: ");
+		scanf("%d", &num);
+		
+	}while (num != -999);
+	
+	
+	return EXIT_SUCCESS;
+}

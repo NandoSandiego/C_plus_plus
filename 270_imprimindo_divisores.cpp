014e4b7a7b0b76126747333c8cf6ec1839f5576c
Fernando Sandiego <mestre.physics@gmail.com>
2022-05-17 15:04:15 -0300

    up 270

--- 270_imprimindo_divisores.cpp
+++ 270_imprimindo_divisores.cpp
@@ -10,6 +10,18 @@ int main(){
 		printf("Digite um numero: ");
 		scanf("%d", &num);
 		
+		for(int i = 2; i <= num %2 == 0 ; i++){
+			
+			if(num % i == 0){
+				printf(" %d ", i);
+				
+				
+			}
+			
+			
+		}
+		printf("\n");
+		
 	}while (num != -999);
 	
 	

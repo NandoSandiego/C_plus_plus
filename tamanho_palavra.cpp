802acade206eea5137daaaa8f819bcb29d9cc2b8
Fernando Sandiego <mestre.physics@gmail.com>
2022-06-16 12:37:31 -0300

    conta vogal

---
+++ tamanho_palavra.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	char palavra[50];
+	int tam_palavra = 0, i = 0;
+	
+	
+	printf("Digite uma palavra: ");
+	scanf("%s", palavra);
+	
+	fflush(stdin);
+	
+	
+		
+		while(palavra[i] != '\0'){
+		i++;
+		tam_palavra += 1;
+	   }
+		
+	
+	
+	
+	
+	
+	printf("%d", tam_palavra);
+	
+	
+	
+	return EXIT_SUCCESS;
+}

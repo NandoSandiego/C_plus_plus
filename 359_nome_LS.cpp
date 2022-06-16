604dfd0a79f210d02143188d5231199139d2c34c
Fernando Sandiego <mestre.physics@gmail.com>
2022-06-16 12:35:52 -0300

    Jogo Forca

--- 359_nome_LS.cpp
+++ 359_nome_LS.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main (){
@@ -11,21 +12,19 @@ int main (){
 	for(int i = 0; i < 3; i++){
 		
 		printf("Digite o nome: ");
-		scanf("%s", &nome[i] );
+		scanf("%s", nome[i] );
 		printf("Digite a idade de %s :", nome[i]);
 		scanf("%d", &idade);
+		
+			if(strncmp(nome[i], "L", 1 )<0  ){
+			nome_LS[i] == nome[i];
+		}
 			
 	}
 	
-	for(int i = 0; i < 3; i++ ){
-		
-		if(nome[i] >= "L" && nome[i] <= "S" ){
-			nome_LS[i] == nome[i];
-		}
-	}
 	
 	for(int i = 0; i < 3; i++){
-		printf("nome[%d] %s \n",i, nome_LS[i]);
+		printf("nome[%d] = %s \n",i, nome_LS[i]);
 	}
 	
 	

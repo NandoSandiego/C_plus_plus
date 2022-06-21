b1df10605d92a711a733a9d2df76152a24e7ba18
ProfNando <mestre.physics@gmail.com>
2022-06-21 19:27:02 -0300

    horoscopo

---
+++ _260_horoscopo_vetorial.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main (){
+	
+	char signo[20][100];
+	int data[100],dia[100], mes[100];
+	
+	
+	
+	for(int i = 0; i < 20; i++){
+		
+		cout << "Digite a data de nascimento(ddmm) " << i+1 <<" Pessoa: ";
+		cin >> data[i];
+		
+		dia[i] = data[i] /100 ;
+		mes[i] = data[i] %100;
+		
+		
+		cout << "dia: " << dia[i] << "\n";
+		cout << "mes: " << mes[i] << "\n";
+		
+		if(mes[i] = 1 && dia[i] <= 20 ){
+			
+			signo[i] == "Capricornio";
+			
+		} else if()
+		
+		cout << signo[i];
+	}
+	
+	
+	
+	return 0;
+}

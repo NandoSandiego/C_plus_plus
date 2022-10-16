a2330884fa4299f4ff52ecc5cd522b1bb3fd212a
Prof. Nando Batista <webdev.physics@gmail.com>
2022-10-16 15:49:50 -0300

    Create 4_variaveis_locais_globais_operadores.cpp

---
+++ Assuntos/4_variaveis_locais_globais_operadores.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// variaveis declarada DENTRO de uma função  - > ESCOPO LOCAl
+// variaveis declarada FORA de uma função   - > ESCOPO GLOBAL
+
+
+int n3,n4; // variaveis globais
+
+int main(){ 
+		
+	int n1, n2, res; // Variaveis locais
+	
+	//OPERADORES MATEMÁTICOS
+	 /*
+	 	+ -> ADIÇÃO
+	 	- -> SUBTRAÇÃO
+	 	/ -> DIVISÃO
+	 	* -> MULTIPLICAÇÃO 
+	 	% -> RESTO DE UMA DIVISÃO
+	 	()-> ORDEM DE PRECEDENCIA DOS OPERADORES
+	 		 
+	 */
+	 
+	 n1 = 11, n2 = 3, n3 = 5, n4 = 2, res;  
+	 
+	 res = n1 + n2 + n3 + n4;
+	 cout << "A soma: " << res;
+	 
+	 res = (n1 + n2 - (n3 *n4)/n1) % 2 ;  
+	
+	
+	return 0;
+}

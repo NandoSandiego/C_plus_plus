a1636bff2b9f483fab3e74864a641f4ea07d1435
Prof. Nando Batista <webdev.physics@gmail.com>
2022-10-16 18:43:41 -0300

    Create 9_Operador_ternario.cpp

---
+++ Assuntos/9_Operador_ternario.cpp
@@ -0,0 +1,30 @@
+/*
+	(expressão) ? verdadeiro : falso
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	float num1,num2, med;
+	string res;
+	
+	cout << "Digite a 1 nota: ";
+	cin >> num1;
+	
+	cout << "Digite a 2 nota: ";
+	cin >> num2;
+	
+	med = (num1 + num2)/2;
+	
+	//med > 7 ? res = "Aprovado": res = "reprovado" ;
+	
+	 res = (med >=7) ? "Aprovado" : "Reprovado"; 
+	
+	cout <<"A media e: "<< med << "\n" <<"O aluno esta " << res;
+	
+	
+	return 0;
+}
+

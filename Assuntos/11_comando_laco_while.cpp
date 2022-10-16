cde3ad313ff14337ba0568c68d9599625210a656
Prof. Nando Batista <webdev.physics@gmail.com>
2022-10-16 19:38:27 -0300

    Create 11_comando_laco_while.cpp

---
+++ Assuntos/11_comando_laco_while.cpp
@@ -0,0 +1,38 @@
+/*
+	- Teste lógico no inicio
+	- Executa enquando a codição for verdadeira
+	
+	while(condição){ 
+	
+		comandos;
+		
+	}
+*/
+
+#include <iostream>;
+using namespace std;
+
+int main(){
+	
+	int n;
+	n = 0;
+	
+	while(n < 10){
+	
+		
+		cout << "num - "<< n << "\n";
+		
+		//cout << "Digite um numero: ";
+		//cin >> n;
+		n++;
+	}
+	
+	cout << "Rotina finalizada.... \n";
+	
+	return 0;
+}
+
+
+
+
+

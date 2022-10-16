68cccfe4b811b5093758bf862fafe52678656a97
Prof. Nando Batista <webdev.physics@gmail.com>
2022-10-16 15:07:13 -0300

    Create 2_variaveis_em_C++.cpp

---
+++ Assuntos/2_variaveis_em_C++.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	/*Variaveis - são posições reservadas 
+		na memoria para armazzenamento de informações 
+		
+		TIPO  NOME; ou 
+		TIPO NOME = VALOR;
+		
+		TIPOS:
+		 - int (inteiro)
+		 - char (caractere)
+		 - float (decimal)
+		 - bolean (boleano)
+		 - string (cadeia de caracteres)
+		 -
+	*/
+	
+	int vidas; // 10,25
+	char letra = 'j'; // 'a', '4', 
+	char letras[20] = "Ola";
+	double decimal; //2.5, 5.588888887
+	float decimal2; //2.5, 5.6
+	bool vivo = true;//true or false
+	string texto = "Fernando";
+	
+	cout << vidas << "\n";
+	cout << letra << "\n";
+	cout << letras << "\n";
+	cout << texto << "\n";
+	
+	
+	return 0;
+}

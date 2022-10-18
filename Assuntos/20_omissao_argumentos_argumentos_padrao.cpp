3d8b1e62fe50a7a6c0dbe6324c70dcf8226a06a0
Prof. Nando Batista <webdev.physics@gmail.com>
2022-10-18 14:44:12 -0300

    Create 20_omissao_argumentos_argumentos_padrao.cpp

---
+++ Assuntos/20_omissao_argumentos_argumentos_padrao.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printar(string txt = "Ola ");
+
+
+int main(){
+	
+	printar();
+	
+	
+	return 0;
+}
+
+
+
+void printar(string txt){
+	cout << "\n" << txt << "\n";
+}

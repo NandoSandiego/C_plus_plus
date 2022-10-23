9ebbc60cd9c86f58851bbad414f5822d5cb3987a
Prof. Nando Batista <webdev.physics@gmail.com>
2022-10-23 10:07:50 -0300

    Create 37_system.cpp

---
+++ Assuntos/37_system.cpp
@@ -0,0 +1,15 @@
+ // executa comandos do sistema
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	
+	system("dir"); // mostra a pasta
+	system("cls"); // limpa a tela
+	system("color 0a"); // formato de codigo
+	system("color ?"); // muda a cor 
+	
+	return 0;
+}

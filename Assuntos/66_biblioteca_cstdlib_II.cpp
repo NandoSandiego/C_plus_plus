709d757498f945b06202d4d90ca9a8fe4a7f6672
Prof. Nando Batista <webdev.physics@gmail.com>
2022-11-02 19:39:11 -0300

    Create 66_biblioteca_cstdlib_II.cpp

---
+++ Assuntos/66_biblioteca_cstdlib_II.cpp
@@ -0,0 +1,39 @@
+/*
+    EXIT_SUCCESS -> Termina pela metade
+*/
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void fim(){
+cout << "FIm do programa " << endl;
+}
+
+
+int main(){
+
+    const char* p;
+    _Exit(EXIT_SUCCESS);
+
+    for(int i = 0; i < 10; i++){
+
+
+        atexit(fim); // Chama a função fim
+        if(i < 5){
+            cout << i << endl;
+        }else{
+            exit(EXIT_SUCCESS); // Necessita de um parametro EXIT_FAILURE = 0, EXIT_SUCCESS = 1
+            cout << i << endl;
+        }
+
+    }
+
+    cout << endl;
+
+/*    p = getenv('PATH');
+    cout << p << endl;
+*/
+
+   return 0;
+}
